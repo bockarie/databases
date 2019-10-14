a45c156b4d7028cc907611aa51e88e8cdacc03c4
bockarie <51052394+bockarie@users.noreply.github.com>
2019-10-14 23:03:54 +0200

    Update program.js

--- week-3-homework/program.js
+++ week-3-homework/program.js
@@ -106,3 +106,95 @@ connectedModel.load((err, result) => {
 });
 
 module.exports = connectedModel;
+
+
+
+
+
+
+
+
+const mysql = require('mysql');
+
+class TodoModel {
+    constructor(dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    // Loads all the TODOs in the database
+    load(callback) {
+        const selectTodoItems = "SELECT * FROM todo_items";
+        this.dbConnection.query(selectTodoItems, function(err, results, fields) {
+            if(err) {
+                callback(err);
+                return;
+            }
+
+            callback(null, results);
+        });
+    }
+
+    create(description, callback) {
+        // Write code and query to create a new TODO item
+        const todoItem = new TodoItem({  });
+        todoList.addTodo(todoItem);
+    }
+
+    update(id, description, callback) {
+        // Write code and query to update and existing TODO item
+
+    delete(id, callback) {
+        // Write code and query to delete an existing TODO item
+        $todo_deleted = todoItem_delete('todo')
+             condition('todoId', 2)
+             execute();
+
+    }
+
+    tagTodoItem(todoItemId, tagId, callback) {
+        // Write code and query add a tag to a TODO item
+        $query  hasTag('node_access');
+    }
+        
+    untagTodoItem(todoItemId, tagId, callback) {
+        // Write code and query remove a tag from a TODO item
+    }
+
+    markCompleted(todoItemId, callback) {
+        // Write code to mark a TODO item as completed
+    }
+}
+
+const dbConnection = mysql.createConnection({
+    host     : 'localhost',
+    user: 'hyfclass5',
+    password : 'H@ckit!',
+    database : 'todo_app'
+});
+
+dbConnection.connect(function(err) {
+    if (err != null) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + dbConnection.threadId);
+
+    const todoModel = new TodoModel(dbConnection);
+    todoModel.load(function(err, todoItems) {
+        if(err) {
+            console.log("error loading TODO items:", err);
+        }
+
+        console.log("existing todo items:", todoItems);
+    });
+});
+
+
+
+
+
+
+
+
+
